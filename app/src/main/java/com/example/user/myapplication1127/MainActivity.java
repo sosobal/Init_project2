ae31bdb6848dd0bbcdb69ea7ba2b5016e2a2111a
sosobal <sosobal1024@naver.com>
2018-11-27 16:50:31 +0900

    finish Sensor

--- app/src/main/java/com/example/user/myapplication1127/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication1127/MainActivity.java
@@ -370,6 +370,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
             Intent intent = new Intent(this, PhoneCallService.class);
             stopService(intent);
         }
+        sensorManager.unregisterListener(mySensorListener);
         super.onDestroy();
     }
 }
--- mypush.bat
+++ mypush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "Start Sensor"
+git commit -m "finish Sensor"
 git push -u origin master
